245cece0f661cd64da4d9be536af91a8ecc32096
Xuqq <Xuqq@yizit.cn>
2018-06-14 13:32:45 +0800

    测试

--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -1,6 +1,7 @@
 #Author: Xqq
 import unittest
 import pytest
+import time
 from selenium import webdriver
 from WEB_Framework_V1.PageObjects.loginpage import LoginPage
 from WEB_Framework_V1.PageObjects.homepage import HomePage
